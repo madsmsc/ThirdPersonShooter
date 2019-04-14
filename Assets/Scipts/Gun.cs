c9db4106168911e8a1519b393e6ac945d7ef5971
Mads Pedersen <mads.msc@gmail.com>
2019-04-14 23:18:26 +0200

    fixed fire sound and muzzle flash and added a bit extra to the map

--- Assets/Scipts/Gun.cs
+++ Assets/Scipts/Gun.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
+public class Gun : MonoBehaviour { 
     public int damage = 1;
     public float fireRate = 1;
+    public ParticleSystem muzzleParticle;
+    public AudioSource gunFire;
 
     private float timer = 0;
 
-    void Update()
-    {
+    void Update() {
         timer += Time.deltaTime;
         if (timer >= fireRate && Input.GetButton("Fire1"))
             fireGun();
     }
 
-    private void fireGun()
-    {
+    private void fireGun() {
         timer = 0;
-        Debug.DrawRay(Camera.main.transform.position, 
-            Camera.main.transform.forward * 100, Color.red, 2);
+        fireEffects();
+        checkRayIntersections();
+    }
+
+    private void checkRayIntersections()
+    {
+        Debug.DrawRay(Camera.main.transform.position,
+    Camera.main.transform.forward * 100, Color.red, 2);
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo, 100))
@@ -31,5 +36,11 @@ public class Gun : MonoBehaviour
             if (health != null)
                 health.takeDamage(damage);
         }
+    }
+
+    private void fireEffects()
+    {
+        muzzleParticle.Play();
+        gunFire.Play();
     }
 }
--- Assets/Scipts/Move.cs
+++ Assets/Scipts/Move.cs
@@ -12,34 +12,38 @@ public class Move : MonoBehaviour
     private CharacterController characterController;
     private Animator animator;
 
+    private static float MAX_MOUSE_DIR = 50;
+    private static float MAX_HEAD_ROTATINO = 20;
+
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
         mouseDir = new Vector2();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        // mouse look
+        mouseLook();
+        keyboardMove();
+    }
+
+    private void mouseLook()
+    {
         mouseDir += new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        // clamp up/down rotation
-        if (mouseDir.y > 2.5f)
-            mouseDir.y = 2.5f;
-        if (mouseDir.y < -2.5f)
-            mouseDir.y = -2.5f;
-        looking.transform.localRotation = Quaternion.AngleAxis(-mouseDir.y * mouseSpeed, Vector3.right);
 
-        // clamp head rotation
-        float headRotation = mouseDir.y;
-        if (headRotation > 1)
-            headRotation = 1;
-        if (headRotation < -1)
-            headRotation = -1;
-        head.transform.localRotation = Quaternion.AngleAxis(-headRotation * mouseSpeed, Vector3.right);
+        float lookingRotation = clamp(-mouseDir.y * mouseSpeed, MAX_MOUSE_DIR);
+        looking.transform.localRotation = Quaternion.AngleAxis(lookingRotation, Vector3.right);
+
+        float headRotation = clamp(-mouseDir.y * mouseSpeed, MAX_HEAD_ROTATINO);
+        head.transform.localRotation = Quaternion.AngleAxis(headRotation, Vector3.right);
         transform.localRotation = Quaternion.AngleAxis(mouseDir.x * mouseSpeed, Vector3.up);
+    }
 
-        // keyboard move/rotate
+    private void keyboardMove()
+    {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var movement = new Vector3(horizontal, 0, vertical);
@@ -48,5 +52,14 @@ public class Move : MonoBehaviour
         {
             characterController.SimpleMove(transform.forward * moveSpeed * vertical);
         }
+    }
+
+    private float clamp(float f, float max)
+    {
+        if (f > MAX_MOUSE_DIR)
+            return MAX_MOUSE_DIR;
+        if (f < -MAX_MOUSE_DIR)
+            return -MAX_MOUSE_DIR;
+        return f;
     }
 }
